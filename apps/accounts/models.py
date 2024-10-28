9b7eaa671fffbe2f46cb835a744ed65e2f171e20
eniiyanu <depopf89@gmail.com>
2024-10-28 10:54:07 +0100

    Created a model to register users

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,71 @@
+# apps/accounts/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    is_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+class Address(models.Model):
+    STATES = [
+        ('AB', 'Abia'),
+        ('FC', 'Abuja'),  
+        ('AD', 'Adamawa'),
+        ('AK', 'Akwa Ibom'),
+        ('AN', 'Anambra'),
+        ('BA', 'Bauchi'),
+        ('BY', 'Bayelsa'),
+        ('BE', 'Benue'),
+        ('BO', 'Borno'),
+        ('CR', 'Cross River'),
+        ('DE', 'Delta'),
+        ('EB', 'Ebonyi'),
+        ('ED', 'Edo'),
+        ('EK', 'Ekiti'),
+        ('EN', 'Enugu'),
+        ('GO', 'Gombe'),
+        ('IM', 'Imo'),
+        ('JI', 'Jigawa'),
+        ('KD', 'Kaduna'),
+        ('KN', 'Kano'),
+        ('KT', 'Katsina'),
+        ('KE', 'Kebbi'),
+        ('KO', 'Kogi'),
+        ('KW', 'Kwara'),
+        ('LA', 'Lagos'),
+        ('NA', 'Nasarawa'),
+        ('NI', 'Niger'),
+        ('OG', 'Ogun'),
+        ('ON', 'Ondo'),
+        ('OS', 'Osun'),
+        ('OY', 'Oyo'),
+        ('PL', 'Plateau'),
+        ('RI', 'Rivers'),
+        ('SO', 'Sokoto'),
+        ('TA', 'Taraba'),
+        ('YO', 'Yobe'),
+        ('ZA', 'Zamfara'),
+    ]
+    
+    state = models.CharField(max_length=2, choices=STATES)
+   
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=3, choices=STATES)
+    is_default = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15)
+    
+    class Meta:
+        verbose_name_plural = 'Addresses'
+        
+    def save(self, *args, **kwargs):
+        if self.is_default:
+            # I want this to set all other addresses of user to non-default
+            Address.objects.filter(user=self.user).update(is_default=False)
+        super().save(*args, **kwargs)
